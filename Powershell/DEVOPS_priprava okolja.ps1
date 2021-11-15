212076263e22e6601941f57b6b7eaca52442a4c7
anze.pirc <anze.pirc@add.si>
2021-11-15 11:59:42 +0100

    dev ops script change

--- Powershell/DEVOPS_priprava okolja.ps1
+++ Powershell/DEVOPS_priprava okolja.ps1
@@ -8,11 +8,12 @@ Kaj mora biti nameščeno (seveda je tega še več):
 -gitLense
 
 Kaj ne sme biti nameščeno:
--AzureRM
+-AzureRM (moramo predhodno odstraniti, da lahko namestimo Azure Powershell)
+https://practicalpowershell.com/remove-azurerm-and-install-az-module-for-azure-powershell-management/
 #>
 
 #uninstall AzureRM
-Get-Module -ListAvailable | Where-Object [$_.Name -like 'AzureRM'] | Uninstall-Module -Force
+Get-Module -ListAvailable | Where-Object {$_.Name -like 'AzureRM'} | Uninstall-Module -Force
 
 #Nameščanje ostalih modulov (gremo po MS dokumentaciji)
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
